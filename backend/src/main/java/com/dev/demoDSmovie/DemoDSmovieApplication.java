c3a733c9397ab6b9275526edea5a64663552c016
morsce <brunolimamorce@gmail.com>
2022-05-11 14:18:38 -0300

    Project Created

---
+++ backend/src/main/java/com/dev/demoDSmovie/DemoDSmovieApplication.java
@@ -0,0 +1,13 @@
+package com.dev.demoDSmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDSmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDSmovieApplication.class, args);
+	}
+
+}
