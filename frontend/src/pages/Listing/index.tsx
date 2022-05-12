4158320e5654d7e899fe010d0b0c63feb6eedebd
morsce <brunolimamorce@gmail.com>
2022-05-12 16:54:55 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,38 @@
+import MovieCard from "components/Navbar/MovieCard";
 import Pagination from "components/Navbar/Pagination";
 
 function Listing() {
 
-return (
-    <Pagination />
-  );
+  return (
+
+    <>
+      <Pagination />
+
+      <div className="container">
+           <div className="row">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>    
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+               </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+               </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard />
+               </div>
+          </div>
+      </div>
+       
+
+   </>
+
+      );
 
 }
 
-export default Listing;
+      export default Listing;
